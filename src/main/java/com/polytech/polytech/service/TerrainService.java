cc5e9d58a7ae1cb87bef10c8b6f8337bc9c9291f
McSIM07 <mmarece17@gmail.com>
2024-11-07 21:57:19 +0100

    Services TerrainService + UtilisateurService

--- src/main/java/com/polytech/polytech/service/TerrainService.java
+++ src/main/java/com/polytech/polytech/service/TerrainService.java
@@ -1,4 +1,41 @@
 package com.polytech.polytech.service;
 
+import com.polytech.polytech.entity.Terrain;
+import com.polytech.polytech.repository.TerrainRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class TerrainService {
+
+    @Autowired
+    private TerrainRepository terrainRepository;
+
+    public List<Terrain> getAllTerrains() {
+        return terrainRepository.findAll();
+    }
+
+    public Terrain getTerrainById(Integer id) {
+        return terrainRepository.findById(id).orElse(null);
+    }
+
+    public Terrain saveTerrain(Terrain terrain) {
+        return terrainRepository.save(terrain);
+    }
+
+    public void deleteTerrain(Integer id) {
+        terrainRepository.deleteById(id);
+    }
+
+    public Terrain updateTerrain(Integer id, Terrain updatedTerrain) {
+        Terrain existingTerrain = getTerrainById(id);
+        if(existingTerrain != null) {
+            existingTerrain.setNom(updatedTerrain.getNom());
+            existingTerrain.setDescription(updatedTerrain.getDescription());
+            existingTerrain.setQuantite(updatedTerrain.getQuantite());
+            existingTerrain.setPoint_geo(updatedTerrain.getPoint_geo());
+            return terrainRepository.save(existingTerrain);
+        }
+        return null;
+    }
 }
