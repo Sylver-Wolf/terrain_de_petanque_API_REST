700f0836e1f83838e1ce5dad23ae6ab6dc7ef47e
waulyn <153715998+waulyn@users.noreply.github.com>
2024-11-07 22:00:32 +0100

    ajout de createUser Ajout des CRUD du controllerTerrain

--- src/main/java/com/polytech/polytech/controller/TerrainController.java
+++ src/main/java/com/polytech/polytech/controller/TerrainController.java
@@ -2,7 +2,7 @@ package com.polytech.polytech.controller;
 
 
 import com.polytech.polytech.entity.Terrain;
-import com.polytech.polytech.repository.TerrainRepository;
+import com.polytech.polytech.service.TerrainService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,16 +10,39 @@ import java.util.List;
 
 
 @RestController
+@RequestMapping("")
 public class TerrainController {
     @Autowired
-    private TerrainRepository terrainRepository;
+    private TerrainService terrainService;
+
     @GetMapping
-    public List<Terrain> getAllAuteurs() {
-        return terrainRepository.findAll();
+    public List<Terrain> getAllTerrain() {
+        return terrainService.getAllTerrains();
     }
+
     @PostMapping
     public Terrain createTerrain(@RequestBody Terrain terrain) {
         Terrain createdTerrain = new Terrain();
+        createdTerrain.setDescription(terrain.getDescription());
+        createdTerrain.setId(terrain.getId());
+        createdTerrain.setNom(terrain.getNom());
+        createdTerrain.setQuantite(terrain.getQuantite());
+        createdTerrain.setPoint_geo(terrain.getPoint_geo());
         return createdTerrain;
+    }
+
+    @GetMapping("/{id}")
+    public Terrain getAuteurById(@PathVariable Long id) {
+        return terrainService.getTerrainById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAuteur(@PathVariable Long id) {
+        terrainService.deleteTerrain(id);
+    }
+
+    @PutMapping("/{id}")
+    public Terrain updateTerrain(@PathVariable Long id, @RequestBody Terrain updatedTerrain) {
+        return terrainService.updateTerrain(id, updatedTerrain);
     }
 }
--- src/main/java/com/polytech/polytech/controller/UserController.java
+++ src/main/java/com/polytech/polytech/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.polytech.polytech.controller;
 
+import com.polytech.polytech.service.UtilisateurService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.polytech.polytech.entity.Utilisateur;
 
 @RestController
 @RequestMapping("")
 public class UserController {
+    @Autowired
+    private UtilisateurService utilisateurService;
 
     @PostMapping("/CreateUser")
     public Utilisateur CreateUser(@RequestBody Utilisateur user) {
@@ -17,4 +21,7 @@ public class UserController {
         createdUser.setMail(user.getMail());
         return createdUser;
     }
+
+
+
 }
